64dc27de6794c83da2da57d8f510e1209cff89d2
MingJ7 <chiamingjiu@yahoo.com.sg>
2019-08-19 14:09:39 +0800

    added form for updating FTView

---
+++ Final Student DHM/DHM_Main/FTView2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV.UI;
+using Emgu.CV;
+
+namespace DHM_Main {
+	public partial class FTView2 : DisplayForm {
+		UMat a = new UMat();
+		public FTView2() {
+			InitializeComponent();
+		}
+	}
+}
